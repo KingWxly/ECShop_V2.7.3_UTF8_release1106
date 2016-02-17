35dbfc0ac524da4171fe02bdcba91aa516609499
Clark <18638155414@163.com>
2016-02-17 20:04:10 +0800

    save

--- upload/data/config.php
+++ upload/data/config.php
@@ -30,6 +30,6 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2016-02-16 18:10:01');
+define('API_TIME', '2016-02-17 19:38:46');
 
 ?>
--- upload/temp/compiled/group_buy.lbi.php
+++ upload/temp/compiled/group_buy.lbi.php
@@ -4,13 +4,13 @@
   <h3><span><?php echo $this->_var['lang']['group_buy_goods']; ?></span><a href="group_buy.php"><img src="themes/default/images/more.gif"></a></h3>
     <div class="centerPadd">
     <div class="clearfix goodsBox" style="border:none;">
-      <?php $_from = $this->_var['group_buy_goods']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'goods_0_58439800_1455699378');if (count($_from)):
-    foreach ($_from AS $this->_var['goods_0_58439800_1455699378']):
+      <?php $_from = $this->_var['group_buy_goods']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'goods_0_04463000_1455708349');if (count($_from)):
+    foreach ($_from AS $this->_var['goods_0_04463000_1455708349']):
 ?>
       <div class="goodsItem">
-           <a href="<?php echo $this->_var['goods_0_58439800_1455699378']['url']; ?>"><img src="<?php echo $this->_var['goods_0_58439800_1455699378']['thumb']; ?>" alt="<?php echo htmlspecialchars($this->_var['goods_0_58439800_1455699378']['goods_name']); ?>" class="goodsimg" /></a><br />
-					 <p><a href="<?php echo $this->_var['goods_0_58439800_1455699378']['url']; ?>" title="<?php echo htmlspecialchars($this->_var['goods_0_58439800_1455699378']['goods_name']); ?>"><?php echo htmlspecialchars($this->_var['goods_0_58439800_1455699378']['short_style_name']); ?></a></p>
-           <font class="shop_s"><?php echo $this->_var['goods_0_58439800_1455699378']['last_price']; ?></font>
+           <a href="<?php echo $this->_var['goods_0_04463000_1455708349']['url']; ?>"><img src="<?php echo $this->_var['goods_0_04463000_1455708349']['thumb']; ?>" alt="<?php echo htmlspecialchars($this->_var['goods_0_04463000_1455708349']['goods_name']); ?>" class="goodsimg" /></a><br />
+					 <p><a href="<?php echo $this->_var['goods_0_04463000_1455708349']['url']; ?>" title="<?php echo htmlspecialchars($this->_var['goods_0_04463000_1455708349']['goods_name']); ?>"><?php echo htmlspecialchars($this->_var['goods_0_04463000_1455708349']['short_style_name']); ?></a></p>
+           <font class="shop_s"><?php echo $this->_var['goods_0_04463000_1455708349']['last_price']; ?></font>
         </div>
       <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
     </div>
