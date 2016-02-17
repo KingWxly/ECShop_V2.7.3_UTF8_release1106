fa1b54530967816b26a68fa4081bef9a73e96158
Clark <18638155414@163.com>
2016-02-17 14:19:35 +0800

    修改数据库配置

--- ectouch/data/config.php
+++ ectouch/data/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-    'DB_HOST'   => 'localhost',
+    'DB_HOST'   => 'localhost:3306',
     'DB_USER'   => 'root',
     'DB_PWD'   => '789456123Abc',
-    'DB_NAME'   => 'xiaomei',
-    'DB_PREFIX'   => 'xm_',
+    'DB_NAME'   => 'ec_demo',
+    'DB_PREFIX'   => 'ecs_',
     'DB_PORT'   => '3306',
     'DB_CHARSET'   => 'utf8',
     'DB_TYPE'   => 'mysql',
