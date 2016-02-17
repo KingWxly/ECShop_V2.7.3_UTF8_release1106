4bdc0eab4e883c23d88370db2177d0591d5d8a39
Clark <18638155414@163.com>
2016-02-17 16:16:25 +0800

    修复 独立站点的图片路径设置

---
+++ update/04 为PREFIX_touch_shop_config表添加配置项IMG_BASE_URL值为PC站的URL.sql
@@ -0,0 +1,3 @@
+INSERT INTO `ecs_touch_shop_config`(`parent_id`, `code`, `type`, `value`, `sort_order`) VALUES (1, 'img_base_url', 'text', 'http://study.ecshop.com/', '1')
+
+/*  删除 ectouch\data\cache\static_caches\touch_shop_config.php 刷新页面重新生成  */
