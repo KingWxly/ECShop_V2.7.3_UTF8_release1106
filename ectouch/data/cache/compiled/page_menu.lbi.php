45ffad32b9b93145a98746ce3fc1b3fd4956c027
Clark <18638155414@163.com>
2016-02-24 19:03:50 +0800

    删除无用代码，更新.gitignore

--- ectouch/data/cache/compiled/page_menu.lbi.php
+++
@@ -1,6 +0,0 @@
-<ul class="ect-diaplay-box text-center">
-  <li class="ect-box-flex"><a href="<?php echo url('index/index');?>"><i class="ect-icon ect-icon-home"></i><?php echo $this->_var['lang']['home']; ?></a></li>
-  <li class="ect-box-flex"><a href="<?php echo url('groupbuy/index');?>"><i class="ect-icon ect-icon-groupbuy"></i><?php echo $this->_var['lang']['groupbuy']; ?></a></li>
-  <li class="ect-box-flex"><a href="<?php echo url('flow/cart');?>"><i class="ect-icon ect-icon-flow"></i><?php echo $this->_var['lang']['shopping_cart']; ?></a></li>
-  <li class="ect-box-flex"><a href="<?php echo url('user/index');?>"><i class="ect-icon ect-icon-user"></i><?php echo $this->_var['lang']['user_center']; ?></a></li>
-</ul>
